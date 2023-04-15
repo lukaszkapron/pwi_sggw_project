f6df44d4faba1950efda9a39dbb23e14b3d27078
Łukasz <lukaskapron@gmail.com>
2023-04-15 15:29:30 +0200

    Change routing way

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import Login from '../src/Pages/Login/Login';
+import Signup from '../src/Pages/Signup/Signup';
+import CreateTeam from '../src/Pages/CreateTeam/CreateTeam';
+import MyTeams from '../src/Pages/MyTeams/MyTeams';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+      <Navbar />   
+          <div>
+        <Routes>
+          <Route path='/pwi_sggw_zad04/CreateTeam' element={<CreateTeam />}></Route>
+          <Route path='/pwi_sggw_zad04/MyTeams' element={<MyTeams />}></Route>
+          <Route path='/pwi_sggw_zad04/Login' element={<Login />}></Route>
+          <Route path='/pwi_sggw_zad04/Signup' element={<Signup />}></Route>
+        </Routes>
+         </div>
+        <Footer />
+      </div>
+    </Router>
+
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,20 @@
 import './Navbar.css'
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import Login from '../../Pages/Login/Login';
-import Signup from '../../Pages/Signup/Signup';
-import CreateTeam from '../../Pages/CreateTeam/CreateTeam';
-import MyTeams from '../../Pages/MyTeams/MyTeams';
 
 
 const Navbar = () => {
 
   return (
     <div>
-      <Router>
         <nav>
           <ul>
-            <li className="left"><Link to='/'>Home</Link></li>
-            <li><Link to='/CreateTeam'>Create Team</Link></li>
-            <li><Link to='/MyTeams'>My Teams</Link></li>
-            <li className="right"><Link to='/Login'>Login</Link></li>
-            <li><Link to='/Signup'>Sign up</Link></li>
+            <li className="left"><Link to='pwi_sggw_zad04/'>Home</Link></li>
+            <li><Link to='pwi_sggw_zad04/CreateTeam'>Create Team</Link></li>
+            <li><Link to='pwi_sggw_zad04/MyTeams'>My Teams</Link></li>
+            <li className="right"><Link to='pwi_sggw_zad04/Login'>Login</Link></li>
+            <li><Link to='pwi_sggw_zad04/Signup'>Sign up</Link></li>
           </ul>
         </nav>
-        <Routes>
-          <Route path='/CreateTeam' element={<CreateTeam />}></Route>
-          <Route path='/MyTeams' element={<MyTeams />}></Route>
-          <Route path='/Login' element={<Login />}></Route>
-          <Route path='/Signup' element={<Signup />}></Route>
-        </Routes>
-      </Router>
     </div>
   )
 }
